22be13b8a23a45997e1f4b2dc96b3572fd3feca5
Stefan Ludwig <stefan.ludwig@slu-it.de>
2019-10-03 19:05:33 +0200

    made start and end optional

--- generate-diff.py
+++ generate-diff.py
@@ -4,10 +4,12 @@ import os
 import os.path
 import re
 import shutil
+import string
 import uuid
 from pathlib import Path
 
 from git import Repo
+from git import Commit
 
 
 class RepositoryDefinition:
@@ -15,32 +17,39 @@ class RepositoryDefinition:
         self.name = str(data['name'])
         self.url = str(data['url'])
         self.active = bool(data['active'])
-        if self.active:
-            self.start = str(dict(data.get('diff')).get('start'))
-            self.end = str(dict(data.get('diff')).get('end', 'HEAD'))
+
+        diff = data.get('diff')
+        self.start = dict(diff).get('start') if diff is not None else None
+        self.end = dict(diff).get('end') if diff is not None else None
 
 
 def execute(configuration: dict):
     working_directory = create_working_directory()
-    for definition in map(RepositoryDefinition, configuration['repositories']):
-        if not definition.active:
-            continue
+    try:
+        for definition in map(RepositoryDefinition, configuration['repositories']):
+            if not definition.active:
+                continue
 
-        print('\n### PROCESSING {} ###\n'.format(definition.name))
+            print()
+            print('### PROCESSING {} ###'.format(definition.name))
+            print()
 
-        repository = clone(working_directory, definition)
+            repository = clone(working_directory, definition)
 
-        relevant_messages = []
-        for commit in commits_between_start_and_end(repository, definition):
-            relevant_messages.append(commit.message)
+            relevant_messages = []
+            for commit in commits_between_start_and_end(repository, definition):
+                relevant_messages.append(commit.message)
 
-        issue_keys = set([])
-        for message in relevant_messages:
-            for found in re.findall(configuration['issueKeyPattern'], message):
-                issue_keys.add(found)
-        print('Issue Keys: {}'.format(', '.join(issue_keys)))
+            issue_keys = set([])
+            for message in relevant_messages:
+                for found in re.findall(configuration['issueKeyPattern'], message):
+                    issue_keys.add(found)
 
-    shutil.rmtree(working_directory)
+            print()
+            print('Issue Keys: {}'.format(', '.join(issue_keys)))
+
+    finally:
+        shutil.rmtree(working_directory)
 
 
 def clone(working_directory: Path, repository: RepositoryDefinition) -> Repo:
@@ -49,22 +58,33 @@ def clone(working_directory: Path, repository: RepositoryDefinition) -> Repo:
 
 
 def commits_between_start_and_end(repository: Repo, definition: RepositoryDefinition, ) -> list:
-    start_commit = repository.commit(definition.start)
-    print('start [{}]: {} - {}'.format(definition.start, start_commit, start_commit.message))
-    end_commit = repository.commit(definition.end)
-    print('  end [{}]: {} - {}'.format(definition.end, end_commit, end_commit.message))
+    start_commit_hexsha = None
+    if definition.start is not None:
+        start_commit = repository.commit(definition.start)
+        start_commit_hexsha = start_commit.hexsha
+        print('start [{}]: {} - {}'.format(definition.start, short_sha(start_commit), start_commit.message))
+    else:
+        print('start [None]')
+
+    end_commit_hexsha = None
+    if definition.end is not None:
+        end_commit = repository.commit(definition.end)
+        end_commit_hexsha = end_commit.hexsha
+        print('  end [{}]: {} - {}'.format(definition.end, short_sha(end_commit), end_commit.message))
+    else:
+        print('  end [None]')
+
     print()
 
     relevant_commits = []
-    for commit in repository.iter_commits(end_commit.hexsha):
+    for commit in repository.iter_commits(end_commit_hexsha):
         relevant_commits.append(commit)
-        if commit.hexsha == start_commit.hexsha:
+        if commit.hexsha == start_commit_hexsha:
             break
     relevant_commits.reverse()
 
     for commit in relevant_commits:
-        print('{}: {}'.format(commit.hexsha, commit.message))
-    print()
+        print('{} - {}'.format(short_sha(commit), commit.message))
 
     return relevant_commits
 
@@ -74,6 +94,10 @@ def get_base_directory() -> Path:
     parser.add_argument('-d', default='.', help='name of the directory to use')
     args = parser.parse_args()
     return Path(str(args.d))
+
+
+def short_sha(commit: Commit) -> string:
+    return commit.hexsha[:10]
 
 
 def create_working_directory() -> Path:
